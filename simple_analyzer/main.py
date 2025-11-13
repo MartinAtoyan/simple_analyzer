33508336a5a9083a769715f256577836629c6366
Martin <martinwork31@gmail.com>
2025-11-13 18:29:25 +0400

    created the main loop

--- simple_analyzer/main.py
+++ simple_analyzer/main.py
@@ -1,4 +1,8 @@
 import os
+import time
+import random
+
+from analyzer import Analyzer
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -7,7 +11,29 @@ config_file = os.environ.get('CONFIG_FILE')
 
 with open(config_file, 'r') as config:
     lines = config.readlines()
-    interval_value = lines[0].split('=')[1].strip()
-    sequence_length_value = lines[1].split('=')[1].strip()
-    print(interval_value)
-    print(sequence_length_value)
+    interval = int(lines[0].split('=')[1].strip())
+    sequence_length = int(lines[1].split('=')[1].strip())
+
+numbers = []
+analyzer = Analyzer(numbers)
+
+while True:
+    num = random.randint(1, 100)
+    analyzer.add_number(num)
+
+    if len(analyzer.array) > sequence_length:
+        analyzer.array.pop(0)
+
+    print(f"\nNew number: {num}")
+    print(f"Numbers: {analyzer.array}")
+    print(f"Even: {analyzer.even_count()} "
+          f" Odd: {analyzer.odd_count()} "
+          f" Increasing pairs: {analyzer.increasing_pairs()} "
+          f" Highest: {analyzer.highest_number()}")
+
+    current_time = time.localtime()
+    if len(analyzer.array) >= sequence_length and current_time.tm_sec == 0:
+        print("\n Stopping: reached full sequence and full minute.")
+        break
+
+    time.sleep(interval)
